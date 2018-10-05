4ab79edff7ba69d60fb4f8b4550fc12bfe30fe24
Phu Pham <anphu.1225@gmail.com>
2018-10-05 17:23:04 +0300

    add training script

---
+++ strain.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+#SBATCH -J train
+#SBATCH --mem-per-cpu 50000
+#SBATCH --gres=gpu:p100:1
+#SBATCH -p gpu
+#SBATCH -t 4:00:00
+
+module purge
+module load intelconda/python3.6-2018.3
+module list
+
+srun python train.py -src_path ./data/hyperspectral_src.pt \
+                     -tgt_path ./data/hyperspectral_tgt.pt \
+                     -gpu 0 \
+                     -epoch 20
+
+echo -e "\n ... \n training ended \n ... \n printing job stats .... \n"
+used_slurm_resources.bash
