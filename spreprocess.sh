cc3752a77c9a50b2040abfe791b875fbef6ac012
Phu Pham <anphu.1225@gmail.com>
2018-10-18 16:46:50 +0300

    fix training script for preprocessing

--- spreprocess.sh
+++ spreprocess.sh
@@ -2,8 +2,8 @@
 
 #SBATCH -J preprocess
 #SBATCH --mem-per-cpu 10000
-##SBATCH --gres=gpu:p100:1
-##SBATCH -p gpu
+#SBATCH --gres=gpu:p100:1
+#SBATCH -p gpu
 #SBATCH -t 1:00:00
 
 #module purge
@@ -15,7 +15,7 @@ module load python-env/intelpython3.6-2018.3 gcc/5.4.0
 module list
 
 
-srun python preprocess.py
+srun python -u preprocess.py
 
 echo -e "\n ... \n training ended \n ... \n printing job stats .... \n"
 used_slurm_resources.bash
--- strain.sh
+++ strain.sh
@@ -2,8 +2,7 @@
 
 #SBATCH -J train
 #SBATCH --mem-per-cpu 10000
-#SBATCH --gres=gpu:k80:1
-##SBATCH --gres=gpu:p100:1
+#SBATCH --gres=gpu:p100:1
 #SBATCH -p gpu
 #SBATCH -t 2:00:00
 
