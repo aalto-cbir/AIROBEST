7d89403555c67a19f4974608c9a1736fbfb3f2b7
Mottus Matti <matti.mottus@vtt.fi>
2018-10-23 22:55:11 +0300

    Added copyright notices

--- tools/borealallometry.py
+++ tools/borealallometry.py
@@ -1,4 +1,12 @@
+"""
+Copyright (C) 2017,2018  Matti Mõttus 
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+You should have received a copy of the GNU General Public License along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
 # Allometric functions for pine, spruce and birch for Finland
+# treespecies numbering as common in forestry practice: 1=pine, 2=spruce 3=birch (can include other broadleaves), etc.
+
 import numpy as np
 
 # missing data using biometry, mostly based on Majasalmi 2013, with exception of crown radius
