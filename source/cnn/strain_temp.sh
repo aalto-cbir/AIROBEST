2528d1f0b083553895491580bca80c29cd3a0f5e
Phu Pham <anphu.1225@gmail.com>
2018-11-06 17:27:44 +0200

    added visdom option

--- source/cnn/strain_temp.sh
+++ source/cnn/strain_temp.sh
@@ -6,6 +6,10 @@
 #SBATCH -p gputest
 #SBATCH -t 0:15:00
 
+##SBATCH --gres=gpu:p100:1
+##SBATCH -p gpu
+##SBATCH -t 1:00:00
+
 id -a
 
 module purge
@@ -18,13 +22,14 @@ python -u train.py  -src_path ../../data/hyperspectral_src_sm_l2norm2.pt \
                     -tgt_path ../../data/hyperspectral_tgt_sm.pt \
                     -metadata ../../data/metadata.pt \
                     -patch_size 35 \
-                    -patch_step 2 \
+                    -patch_step 3 \
                     -lr 0.001 \
                     -batch_size 64 \
                     -epoch 50 \
                     -model ChenModel \
-                    -save_dir Chen-311018-test1 \
+                    -save_dir Chen-0511018-test1 \
                     -report_frequency 5 \
+                    -visdom_server http://taito-gpu.csc.fi \
                     -gpu 0
 
 echo -e "\n ... printing job stats .... \n"
