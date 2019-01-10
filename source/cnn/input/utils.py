fixed input normalization

source/cnn/input/utils.py
@@ -3,6 +3,8 @@ from PIL import Image
 from sklearn.model_selection import train_test_split
 import torch
 import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set()
 
 
 def envi2world_p(xy, GT):
@@ -119,16 +121,25 @@ def split_data(rows, cols, norm_inv, patch_size, stride=1, mode='grid'):
     return train, val
 
 
-def visualize_label(hyper_labels):
-    _, _, B = hyper_labels.shape
-    flatten_labels = hyper_labels.reshape(-1, B)
-    for i in range(B):
-        y_values = flatten_labels[:, i]
-        plt.bar(len(flatten_labels), y_values)
-
-    plt.savefig('./data/labels-bar.jpg')
-    # TODO: pie charts for classes
-    plt.show()
+def visualize_label(hyper_labels, save_path):
+    """
+    Visualize normalized hyper labels
+    :param hyper_labels:
+    :return:
+    """
+    n_tasks = 13
+    label_names = ['basalarea', 'meanage', 'stemcount', 'meanheight', 'N_strata', 'percentage_mainspecies',
+                   'percentage_pine', 'percentage_spruce', 'percentage_broadleaf', 'woodybiomass', 'LAI*100',
+                   'LAI_effective*100', 'dbh*100']
+    reg_labels = hyper_labels[:, :, -n_tasks:]
+    print(reg_labels.shape, reg_labels.shape[-1])
+    for i in range(reg_labels.shape[-1]):
+        labels = reg_labels[:, :, i]
+        ax = sns.heatmap(labels)
+        figure = ax.get_figure()
+        figure.savefig('{}/{}'.format(save_path, label_names[i]))
+        plt.close(figure)
+        print('{}: Min={}, Max={}, Mean={}'.format(label_names[i], np.min(labels), np.max(labels), np.mean(labels)))
 
 
 def save_as_rgb(hyper_image, wavelength, path):
@@ -144,6 +155,7 @@ def save_as_rgb(hyper_image, wavelength, path):
     i_b = abs(wavelength - 0.490).argmin()  # blue, closest to 490 nm
 
     hyp_rgb = hyper_image[:, :, [i_r, i_g, i_b]]
-    hyp_rgb = hyp_rgb * 255 / np.max(hyp_rgb)
+    print(np.max(hyp_rgb))
+    hyp_rgb = hyp_rgb / 40
     # hyp_rgb = np.asarray(np.copy(hyp_rgb).transpose((2, 0, 1)), dtype='float32')
     Image.fromarray(hyp_rgb.astype('uint8')).save('%s/rgb_image.png' % path)

source/cnn/models/model.py
@@ -251,7 +251,7 @@ class PhamModel(nn.Module):
 
 
 class ModelTrain(nn.Module):
-    def __init__(self, model, criterion_cls, criterion_reg, metadata):
+    def __init__(self, model, criterion_cls, criterion_reg, metadata, options):
         super(ModelTrain, self).__init__()
         self.model = model
         self.task_count = model.n_cls + model.n_reg
@@ -259,15 +259,22 @@ class ModelTrain(nn.Module):
         self.criterion_cls = criterion_cls
         self.criterion_reg = criterion_reg
         self.categorical = metadata['categorical']
+        self.options = options
 
     def forward(self, src, tgt_cls, tgt_reg):
         task_loss = []
         pred_cls, pred_reg = self.model(src)
         start = 0
+
         for key, values in self.categorical.items():
             n_classes = len(values)
             prediction, target = pred_cls[:, start:(start+n_classes)], tgt_cls[:, start:(start+n_classes)]
-            single_loss = self.criterion_cls(prediction, target)
+            # target = torch.argmax(target, dim=1).long()
+            if self.options.disabled == 'classification':
+                single_loss = torch.tensor(0.0, device=tgt_cls.device)
+            else:
+                single_loss = self.criterion_cls(prediction, target)
+
             task_loss.append(single_loss)
             start += n_classes
 

source/cnn/preprocess.py
@@ -13,7 +13,7 @@ import spectral
 import torch
 import matplotlib.pyplot as plt
 
-from input.utils import hyp2for, world2envi_p, save_as_rgb
+from input.utils import hyp2for, world2envi_p, save_as_rgb, visualize_label
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__)))))
 from tools.hypdatatools_img import get_geotrans
@@ -28,7 +28,6 @@ def parse_args():
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('-hyper_data_path',
                         required=False, type=str,
-                        # default='/proj/deepsat/hyperspectral/subset_A_20170615_reflectance.hdr',
                         default='/proj/deepsat/hyperspectral/20170615_reflectance_mosaic_128b.hdr',
                         help='Path to hyperspectral data')
     parser.add_argument('-forest_data_path',
@@ -218,25 +217,30 @@ def process_labels(labels, save_path):
     # delete all the categorical classes from label data
     labels = np.delete(labels, np.append(categorical_classes, useless_bands), axis=2)
 
+    fig.savefig('%s/class_distribution.png' % save_path)
+    plt.close(fig)
+
     # normalize data for regression task
+    normalized_labels = []
     for b in range(labels.shape[2]):
         max = np.max(labels[:, :, b])
         min = np.min(labels[:, :, b])
         if max != min:
-            labels[:, :, b] = (labels[:, :, b] - min) / (max - min)
+            normalized_labels.append((labels[:, :, b] - min) / (max - min))
         elif max != 0:  # if all items have the same non-zero value
-            labels[:, :, b].fill(0.5)
+            normalized_labels.append(labels[:, :, b].fill(0.5))
         else:  # if all are 0, if this happens, consider remove the whole band from data
-            labels[:, :, b].fill(0.0)
+            normalized_labels.append(labels[:, :, b].fill(0.0))
             print('Band with index %d has all zero values, consider removing it!' % b)
 
+    normalized_labels = np.stack(normalized_labels, axis=2)
     # concatenate with newly transformed data
-    labels = np.concatenate((transformed_data, labels), axis=2)
+    normalized_labels = np.concatenate((transformed_data, normalized_labels), axis=2)
 
     metadata['categorical'] = categorical
     metadata['num_classes'] = num_classes
-    fig.savefig('%s/class_distribution.png' % save_path)
-    return labels, metadata
+
+    return normalized_labels, metadata
 
 
 def main():
@@ -263,6 +267,8 @@ def main():
     # Disable human data for now as there are only 19 Titta points in the map
     # hyper_labels = apply_human_data(options.human_data_path, hyper_labels, hyper_gt, forest_columns)
     hyper_labels, metadata = process_labels(hyper_labels, save_path)
+
+    visualize_label(hyper_labels, save_path)
 
     image_norm_name = '%s/image_norm_%s.pt' % (save_path, options.normalize_method)
     tgt_name = '%s/%s.pt' % (save_path, options.tgt_file_name)

source/cnn/train.py
@@ -37,6 +37,10 @@ def parse_args():
                         required=False, type=str,
                         default='./data/mosaic/hyperspectral_tgt_normalized.pt',
                         help='Path to training labels')
+    parser.add_argument('-input_normalize_method',
+                        required=False, type=str,
+                        default='l2_norm', choices=['l2_norm', 'minmax_scaling'],
+                        help='Normalization method for input image')
     parser.add_argument('-metadata',
                         type=str,
                         default='./data/mosaic/metadata.pt',
@@ -116,6 +120,9 @@ def main():
     checkpoint = None
     options = parse_args()
 
+    # TODO: options
+    options.disabled = 'classification'
+
     if options.train_from:
         print('Loading checkpoint from %s' % options.train_from)
         print('Overwrite some options with values from checkpoint!!!')
@@ -178,6 +185,9 @@ def main():
     # loss = nn.CrossEntropyLoss()
     # loss = nn.MultiLabelSoftMarginLoss(size_average=True)
 
+    # TODO: refactor
+    if options.input_normalize_method == 'minmax_scaling':
+        norm_inv = None
     train_loader = get_loader(hyper_image,
                               norm_inv,
                               hyper_labels_cls,
@@ -200,7 +210,7 @@ def main():
                             shuffle=True)
 
     print('Dataset sizes: train={}, val={}'.format(len(train_loader.dataset), len(val_loader.dataset)))
-    modelTrain = ModelTrain(model, loss_cls, loss_reg, metadata)
+    modelTrain = ModelTrain(model, loss_cls, loss_reg, metadata, options)
     # do this before defining the optimizer:  https://pytorch.org/docs/master/optim.html#constructing-it
     modelTrain = modelTrain.to(device)
     optimizer = optim.Adam(modelTrain.parameters(), lr=options.lr)

source/cnn/trainer.py
@@ -291,7 +291,11 @@ class Trainer(object):
         avg_accuracy = torch.mean(val_accuracies)
         conf_matrices = []
         for i in range(tgt_indices.shape[-1]):
-            conf_matrices.append(confusion_matrix(tgt_indices[:, i], pred_indices[:, i]))
+            conf_matrix = confusion_matrix(tgt_indices[:, i], pred_indices[:, i])
+            # convert to percentage along rows
+            conf_matrix = conf_matrix / conf_matrix.sum(axis=1, keepdims=True)
+            conf_matrix = 100*np.around(conf_matrix, decimals=2)
+            conf_matrices.append(conf_matrix)
         return average_loss, avg_accuracy, val_accuracies, conf_matrices
 
     def compute_accuracy(self, predict, tgt):