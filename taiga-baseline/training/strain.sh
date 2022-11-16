df5debe1c937745867d493bcfa37ecc9f416a510
Jorma Laaksonen <jorma.laaksonen@aalto.fi>
2022-11-16 14:40:35 +0200

    Updated spreprocess.sh.

--- taiga-baseline/training/strain.sh
+++ taiga-baseline/training/strain.sh
@@ -1,17 +1,19 @@
 #! /usr/bin/env bash
 
-#SBATCH -J train5
-#SBATCH --mem-per-cpu 150000
+#SBATCH -J train
+#SBATCH --account=project_2001284
 #SBATCH --gres=gpu:v100:1
 #SBATCH -p gpu
 #SBATCH -t 72:00:00
-#SBATCH --account=project_2001284
+#SBATCH --mem-per-cpu 150000
 
-id -a
-module purge
-module load pytorch/1.10
-module list
+TAIGA_DIR=/scratch/project_2001284/TAIGA
 DATA_DIR=../data/TAIGA
+
+module -q purge
+module load pytorch
+module list
+
 . ../venv/bin/activate
 
 python3 -u train.py \
@@ -19,7 +21,7 @@ python3 -u train.py \
 	    -src_norm_multiplier ${DATA_DIR}/image_norm_l2norm_along_channel.pt \
 	    -tgt_path ${DATA_DIR}/hyperspectral_tgt_normalized.pt \
 	    -metadata ${DATA_DIR}/metadata.pt \
-        -hyper_data_header /scratch/project_2001284/TAIGA/20170615_reflectance_mosaic_128b.hdr \
+            -hyper_data_header ${TAIGA_DIR}/20170615_reflectance_mosaic_128b.hdr \
 	    -data_split_path ${DATA_DIR}/data-split \
 	    -save_dir test_model \
 	    -report_frequency 100 \
@@ -34,24 +36,12 @@ python3 -u train.py \
 	    -patch_size 45 \
 	    -patch_stride 13 \
 	    -batch_size 32 \
-        -use_visdom \
-        -visdom_server http://puhti-login2.csc.fi \
-        -train_from ../checkpoint/test_model/model_e136_0.04546.pt
-    
-# -keep_best 10 -visdom_server http://puhti-login2.csc.fi/ -ignored_cls_tasks 4 5 6 7 8 -ignored_reg_tasks 0 1 2 3 4 5 6 7 8 9 10 11 12
-# -ignored_cls_tasks 4 5 6 7 8 -ignored_reg_tasks 0 1 2 3 4 5 6 7 8 9 10 11 12
-# -ignored_cls_tasks 0 1 2 3 4 5 6 7 8
-# -ignored_reg_tasks 4 5 6 10 11 12
+	    -use_visdom \
+	    -visdom_server http://puhti-login2.csc.fi \
+	    -train_from ../checkpoint/test_model/model_e136_0.04546.pt
 
-# 
-# -use_visdom 
+if [[ -v SLURM_JOB_ID ]]; then
+    echo -e "\n ... printing job stats .... \n"
+    seff $SLURM_JOB_ID
+fi
 
-#-train_from ./checkpoint/FL_Pham34-18012021-gn-aug-eelis-patchsize91-set1-flip/model_e96_0.09109.pt
-# -ignored_cls_tasks 0 1 2 9 -ignored_reg_tasks 0 1 2 3 4 5 6 7 8 10 11 12 13 14
-
-#                    -ignored_cls_tasks 0 1
-#                    -ignored_reg_tasks 0 1 2 3 4 5 6 7 9 10 11 12 13 14
-
-echo -e "\n ... printing job stats .... \n"
-
-seff $SLURM_JOB_ID
